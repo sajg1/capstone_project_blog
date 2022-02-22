b1ba0b0fe33417abff0cdc6ccb8e93d724d82a85
Steve Gossip <stephengossip@gmail.com>
2022-02-22 22:47:04 +0000

    added some jinja in order to loop through the blog api and show the title and subtitle on the home page with a 'Read' link. When the read link is pressed, the user is taken to the full blog post.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get('https://api.npoint.io/89579a9c020f1d21c96e')
+    blog_posts = response.json()
+    return render_template("index.html", posts=blog_posts)
+
+@app.route("/blog/<id>/<title>/<subtitle>/<body>")
+def get_blog(id, title, subtitle, body):
+    print(type(title))
+    print(title)
+    return render_template("post.html", post_title=title, post_sub=subtitle, post_bod=body)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
