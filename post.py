b416a62058c1905175347f955691ea0db5d70097
Steve Gossip <stephengossip@gmail.com>
2022-02-23 13:49:04 +0000

    created a Post class that takes in all the parameters for a blog post from the api.

--- post.py
+++ post.py
@@ -1,2 +1,15 @@
-class Post:
-    pass
+class Post():
+    def __init__(self, id, title, subtitle, body):
+        self.id = id
+        self.title = title
+        self.subtitle = subtitle
+        self.body = body
+
+    def get_title(self):
+        return self.title
+
+    def get_subtitle(self):
+        return self.subtitle
+
+    def get_body(self):
+        return self.body
